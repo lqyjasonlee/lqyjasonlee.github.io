d1fa3a769beba6f9c144d78df4b649625e92577f
Qiyuan Li <lqyjasonlee@gmail.com>
2023-02-18 17:01:30 +0800

    Update publications.py

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -79,9 +79,6 @@ for row, item in publications.iterrows():
     if len(str(item.excerpt)) > 5:
         md += "\nexcerpt: '" + html_escape(item.excerpt) + "'"
     
-    md += "\ndate: " + str(item.pub_date) 
-    
-    md += "\nvenue: '" + html_escape(item.venue) + "'"
     
     if len(str(item.paper_url)) > 5:
         md += "\npaperurl: '" + item.paper_url + "'"
@@ -98,7 +95,6 @@ for row, item in publications.iterrows():
     if len(str(item.excerpt)) > 5:
         md += "\n" + html_escape(item.excerpt) + "\n"
         
-    md += "\nRecommended citation: " + item.citation
     
     md_filename = os.path.basename(md_filename)
        
